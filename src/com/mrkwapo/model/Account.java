e51382a4814bf882bd6a53427ac0dcdff8bbab25
mrkwapo <mrkwapo@gmail.com>
2020-08-02 16:13:02 -0400

    updated

--- src/com/mrkwapo/model/Account.java
+++ src/com/mrkwapo/model/Account.java
@@ -1,4 +1,17 @@
 package com.mrkwapo.model;
 
 public class Account {
+    private int balance = 10_000;
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void withdraw(int amount){
+        balance = balance - amount;
+    }
+
+    public void deposit(int amount){
+        balance = balance - amount;
+    }
 }
--- src/com/mrkwapo/worker/AccountHolder.java
+++ src/com/mrkwapo/worker/AccountHolder.java
@@ -1,4 +1,36 @@
 package com.mrkwapo.worker;
 
-public class AccountHolder {
+import com.mrkwapo.model.Account;
+
+public class AccountHolder implements Runnable{
+    private Account account;
+
+    public AccountHolder(Account account) {
+        this.account = account;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 1; i <= 4; i++) {
+            makeWithdrawl(2000);
+            if(account.getBalance() < 0) {
+                System.out.println("You're account is overdrawn!");
+            }
+        }
+    }
+
+    private void makeWithdrawl(int withdrawAmount) {
+        if (account.getBalance() >= withdrawAmount) {
+            System.out.println(Thread.currentThread().getName() + " is going to withdraw $" + withdrawAmount);
+            try{
+                Thread.sleep(3000);
+            }catch (InterruptedException ex){
+
+            }
+            account.withdraw(withdrawAmount);
+            System.out.println(Thread.currentThread().getName() + " completes the withdrawal of $" + withdrawAmount);
+        } else {
+            System.out.println("Not enough in account for " + Thread.currentThread().getName() + " to withdraw from " + account.getBalance());
+        }
+    }
 }
